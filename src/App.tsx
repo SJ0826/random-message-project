2afbbbee542e44e3ff8f8bad258a3687dae27ca3
sujin <ikosdu60@gmail.com>
2023-04-13 19:56:47 +0900

    Chore: set title logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import GetStart from 'pages/GetStart'
 import React from 'react'
 
 function App() {
-	return <div className="App">test</div>
+	return <GetStart />
 }
 
 export default App
---
+++ src/pages/GetStart/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'style/main.css'
+
+const GetStart = () => {
+	return (
+		<div className="start__container">
+			<span className="start__title">✉ 메세지가 도착했습니다</span>
+		</div>
+	)
+}
+
+export default GetStart
