0af4590b5a5a86a9a3c3c85f500a3b2bcb7d6e97
sujin <ikosdu60@gmail.com>
2023-04-13 23:32:08 +0900

    Feat: apply page layout

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessageList = () => {
+	return (
+		<section className="messageList__container">
+			<div className="messageList__header">messageList header</div>
+		</section>
+	)
+}
+
+export default MessageList
