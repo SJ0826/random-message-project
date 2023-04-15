53716c410d044c8a78bbcca30dd45eb0db70c551
sujin <ikosdu60@gmail.com>
2023-04-15 18:50:54 +0900

    Feat: received message UI & send message button UI

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+const dummy = [
+	{ name: '성진', message: 'hi' },
+	{ name: '영현', message: 'hi' },
+	{ name: '원필', message: 'hi' },
+	{ name: '도운', message: 'hi' },
+]
+
 const MessageList = () => {
 	return (
 		<section className="messageList__container">
-			<div className="messageList__header">messageList header</div>
+			<div className="messageList__header">
+				<div className="received__message">
+					<span>받은 메세지</span>
+					<div className="received__message__number">{dummy.length}</div>
+				</div>
+				<div className="send__message__button">메세지 보내기</div>
+			</div>
+
+			<div className="messageList_list">messageList Content</div>
 		</section>
 	)
 }
