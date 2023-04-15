952c644617ade77d08b922b81a2c099edef1e627
sujin <ikosdu60@gmail.com>
2023-04-15 23:46:54 +0900

    Feat: add github link in footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<a
+				className="github__link"
+				href="https://github.com/SJ0826/random-message-project"
+			>
+				<div className="github__link__icon" />
+				<span className="github__text">GitHub</span>
+			</a>
+		</div>
+	)
+}
+
+export default Footer
